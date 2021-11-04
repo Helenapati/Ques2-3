dfacb9db5ee4db4ac80d6861fd379528bd164cae
Helenapati <88396481+Helenapati@users.noreply.github.com>
2021-11-03 22:41:56 -0300

    Helena Patrícia de Souza Silva - 2ºA

---
+++ Main (5).java
@@ -0,0 +1,79 @@
+public class Banco{
+    
+    public static vaid main (String []args);
+    ContaTesouroDireto ct = new ContaTesouroDireto();
+    
+     public class ContaCorrente{
+        private int saldo; 
+    
+      public class ContaCorrente1{
+        int[] a =  new int [1];
+        int [ ]b=new int [2];
+        int []c = new int [3];
+        int []d =  new int [4];
+        int []e = new int [5]; 
+        int []f = new int [6];
+        int []g = new int [7];
+        int [ ]h= new int [8 ];
+        int []i= new int [9];
+        int []j = new int [10];
+        
+        int[ ]array = [2,4,6,8,10,12,14,16,18,20]
+    } 
+     }
+
+    public class ContaTesouroDireto extends ContaCorrente{
+        public saldo (int saldo){
+            int saldo= 10000;
+            array= new int [1];
+            array = 2;
+            
+            System.out.println ("Valor é :"+ saldo );
+           
+            
+        }
+        
+        
+    }
+
+    
+    public class ContaTesouroDireto extends ContaCorrente{
+        public saldo (int saldo){
+            int saldo= 10000;
+            array= new int [2];
+           array= 4;
+            System.out.println ("Valor é :"+ saldo );
+           
+            
+        }
+        
+     public class Depossito extends ContaCorrente1{
+         int[1] a = 2;
+         deposito = 500;
+         x= 2+500;
+      System.out.println ("Valor é :"+ x);
+           
+    }        
+      public class Depossito extends ContaCorrente1{
+         int[2] a = 4;
+         deposito = 500;
+         x= 4+500;
+      System.out.println ("Valor é :"+ x);
+     }       
+    
+    public class Juros extends ContaCorrente1{
+        int[1] a= 2;
+        int= 1;
+        x= (2(1/100));
+        System.out.println("Valor é:"+ x);
+    }
+
+        public class Juros extends ContaCorrente1{
+        int[2] a= 4;
+        int= 1;
+        x= (4(1/100));
+        System.out.println("Valor é:"+ x);
+    }
+    }
+
+
